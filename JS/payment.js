d4bb65a9f79a8965f374824ba520d78155e325c4
jesperkarlsson42 <jesper.karlsson@medieinstitutet.se>
2020-12-07 11:11:10 +0100

    små fix

--- JS/payment.js
+++ JS/payment.js
@@ -9,25 +9,25 @@ $(function () {
         let cvc = $('#cvc').val();
 
         if (fname.length <3 || fname == '') {
-            $('#form-fname').addClass('errorFirstName');
+            $('#form-fname').addClass('errorValidator');
         }
         
         if (lname.length <3 || lname == '') {
-            $('#form-lname').addClass('errorLastName');
+            $('#form-lname').addClass('errorValidator');
         }
 
         if (cardNumber.length < 16 || cardNumber == '' || cardNumber.length > 16) {
-            $('#cardNumber').addClass('errorCardNumber');
+            $('#cardNumber').addClass('errorValidator');
         }
         if (month.length < 2 || month.length > 2 || month > 12) {
-            $('#month').addClass('errorCardNumber');
+            $('#month').addClass('errorValidator');
         }
         if (year.length < 2 || year.length > 2 || year < 20) {
-            $('#year').addClass('errorCardNumber');
+            $('#year').addClass('errorValidator');
         }
 
         if (cvc.length < 3 || cvc.length > 3 ||!/^[0-9]+$/.test(cvc)) {
-            $('#cvc').addClass('errorCardNumber');
+            $('#cvc').addClass('errorValidator');
         }
 
         else {     
