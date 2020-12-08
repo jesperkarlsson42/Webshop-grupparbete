5ae62cbacf3fd03945f1d237fb31fd9b40564f5d
Carolina Lagerberg <carolina.lagerberg@live.se>
2020-12-08 10:42:29 +0100

    finished delete function shopping cart

--- JS/products.js
+++ JS/products.js
@@ -1,6 +1,8 @@
+let x = 0;
+
 class Product {
-  constructor(id, name, price, image) {
-    this.id = id;
+  constructor(name, price, image) {
+    this.id = x++;
     this.name = name;
     this.price = price;
     this.image = image;
@@ -10,66 +12,36 @@ class Product {
 let products = [];
 let cartProducts = [];
 
-let p1 = new Product(
-  "R1",
-  "Rolex",
-  14245,
-  "<img src='./../img/rolexsilver.jpg'/>"
-);
-let p2 = new Product(
-  "G2",
-  "Gant",
-  1145,
-  "<img src='./../img/gantsilver.png'/>"
-);
-let p3 = new Product(
-  "T3",
-  "Timex",
-  1395,
-  "<img src='./../img/timexbrown.jpg'/>"
-);
+let p1 = new Product("Rolex", 14245, "<img src='./../img/rolexsilver.jpg'/>");
+let p2 = new Product("Gant", 1145, "<img src='./../img/gantsilver.png'/>");
+let p3 = new Product("Timex", 1395, "<img src='./../img/timexbrown.jpg'/>");
 let p4 = new Product(
-  "L4",
   "Longines",
   10725,
   "<img src='./../img/longinessilver.jpg'/>"
 );
 let p5 = new Product(
-  "H5",
   "Hunters Race",
   2495,
   "<img src='./../img/huntersbrown.jpg'/>"
 );
 let p6 = new Product(
-  "T6",
   "Tommy Hilfiger",
   1595,
   "<img src='./../img/tommyblack.jpg'/>"
 );
 let p7 = new Product(
-  "EA7",
   "Emporio Armani",
   2745,
   "<img src='./../img/armanisilver.jpg'/>"
 );
-let p8 = new Product(
-  "F8",
-  "Fossil",
-  1295,
-  "<img src='./../img/fossilsilver.jpg'/>"
-);
+let p8 = new Product("Fossil", 1295, "<img src='./../img/fossilsilver.jpg'/>");
 let p9 = new Product(
-  "TS9",
   "Thomas Sabo",
   2625,
   "<img src='./../img/thomasblack.jpg'/>"
 );
-let p10 = new Product(
-  "B10",
-  "Braun",
-  2495,
-  "<img src='./../img/braunblack.jpg'/>"
-);
+let p10 = new Product("Braun", 2495, "<img src='./../img/braunblack.jpg'/>");
 
 $(function () {
   addProduct();
@@ -111,7 +83,7 @@ function createProduct() {
     $("<h3>").html(product.name).appendTo(container);
     $("<p>").html(product.price).appendTo(container);
     let addToCartButtons = $("<button>Add to Cart</button>")
-      .attr("id", "AddToCartButton")
+      .addClass("AddToCartButton")
       .appendTo(container);
     addToCartButtons.on("click", { p: product }, clickedAddToCart);
 
@@ -128,7 +100,7 @@ function createShoppingCart() {
     console.log(cartProduct);
 
     let shoppingCartContainer = $("<div>")
-      .addClass("Cartproduct")
+      .addClass("cartproduct")
       .attr("id", cartProduct.id);
 
     $("<div>")
@@ -138,12 +110,23 @@ function createShoppingCart() {
     $("<h3>").html(cartProduct.name).appendTo(shoppingCartContainer);
     $("<p>").html(cartProduct.price).appendTo(shoppingCartContainer);
     let deleteButton = $("<button>Delete</button>")
-      .attr("id", "deleteButton")
+      .addClass("deleteButton")
       .appendTo(shoppingCartContainer);
-    deleteButton.on("click", { p: cartProduct }, deleteCartProduct);
+    deleteButton.on("click", { c: cartProduct }, deleteCartProduct);
 
     shoppingCartContainer.appendTo($("#shoppingCart-container"));
   });
+}
+
+function deleteCartProduct(e) {
+  for (let i = 0; i < cartProducts.length; i++) {
+    $(".product").html(e.data.c);
+    console.log(e.data.c);
+    if (cartProducts[i].id == e.data.c.id) {
+      cartProducts.splice(i, 1);
+    }
+    createShoppingCart();
+  }
 }
 
 function clickedAddToCart(e) {
@@ -155,37 +138,11 @@ function clickedAddToCart(e) {
   updateCartTotalPrice();
 }
 
-// function deleteCartProduct(e) {
-//   $.each(cartProducts, (i, p) => {
-//     $(".product").html(e.data.p);
-//     console.log(e.data.p);
-//     let removed = cartProducts.splice(i, 1);
-//     console.log(cartProducts);
-//     console.log(removed);
-//   });
-//   createShoppingCart();
-// }
+// listOfTotal = [];
 
-function deleteCartProduct(e) {
-  $.each(cartProducts, (i, p) => {
-    if (cartProducts[i].name == p.name) {
-      console.log(e.data.p);
-      let removed = cartProducts.splice(i, 1);
-      console.log(cartProducts);
-      console.log(removed);
-    }
-  });
-  createShoppingCart();
-}
-
-// function deleteTodo(todo) {
-//   for (let i = 0; i < todos.length; i++) {
-//     if (todos[i].name == todo.name) {
-//       todos.splice(i, 1);
-//     }
-//     createHTML();
-//     addToLocalStorage(todos);
-//   }
+// for (let p = 0; p < cartProducts.length; p++) {
+//   total = cartProducts[p].price;
+//   listOfTotal.push(total);
 // }
 
 function updateCartTotalPrice() {
